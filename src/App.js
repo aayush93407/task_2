802473486c23864adf0a38a6847733512c7b8f32
aayush93407 <92848064+aayush93407@users.noreply.github.com>
2024-10-10 23:56:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,12 @@ const App = () => {
   const handleDateSubmit = (e) => {
     e.preventDefault();
     alert("Correct");
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   };
 
   return (
     <div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       <header className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
         <a href="/" className="d-flex align-items-center link-body-emphasis text-decoration-none">
           <div style={{ backgroundColor: "wheat" }}>
@@ -83,6 +85,7 @@ const App = () => {
 
 const ProjectCard = ({ title, description, details, onClick }) => (
   <div className="col">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <div className="card mb-4 rounded-3 shadow-sm border-primary">
       <div className="card-header py-3 text-bg-primary border-primary">
         <h4 className="my-0 fw-normal">{title}</h4>
